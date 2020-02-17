0127b108647d9020d67313dfe7ff266e070e41b7
Matthew Pitkin <matthew.pitkin@ligo.org>
2020-02-17 11:17:41 +0000

    __init__.py: remove hack to stop bilby logging info

--- cwinpy/__init__.py
+++ cwinpy/__init__.py
@@ -1,37 +1,10 @@
-### import bilby and bilby_pipe removing the logger version info output ###
 import io
-import logging
-
 
 # register reader/writer
 from . import iostream
 from ._version import get_versions
 from .data import HeterodynedData, MultiHeterodynedData
 from .likelihood import TargetedPulsarLikelihood
-
-bilby_logger = logging.getLogger("bilby")
-handler = logging.StreamHandler(io.StringIO())
-bilby_logger.addHandler(handler)
-import bilby
-
-bilby_logger.removeHandler(handler)
-# reset bilby logging handler to default
-from bilby.core.utils import setup_logger as bilby_setup_logger
-
-bilby_setup_logger()
-bilby_pipe_logger = logging.getLogger("bilby_pipe")
-bilby_pipe_logger.addHandler(handler)
-import bilby_pipe
-
-bilby_pipe_logger.removeHandler(handler)
-# reset bilby_pipe logging handler to default
-from bilby_pipe.utils import setup_logger as bilby_pipe_setup_logger
-
-bilby_pipe_setup_logger()
-###
-
-
-
 
 __version__ = get_versions()["version"]
 del get_versions
