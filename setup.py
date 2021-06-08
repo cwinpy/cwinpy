be78e274315679086e4ac47d178276285d5171cf
Matthew Pitkin <matthew.pitkin@ligo.org>
2021-06-08 23:38:34 +0100

    Simplify Cython usage in setup.py - as Cython is a build requirement we do not need to test for its existence

--- setup.py
+++ setup.py
@@ -5,52 +5,34 @@ A Python module for Bayesian inferences with continuous gravitational wave
 sources.
 """
 
+import numpy as np
 from setuptools import setup
 from setuptools import Extension
 
-import numpy
 
-
-# check whether user has Cython
-try:
-    import Cython  # noqa: F401
-except ImportError:
-    have_cython = False
-else:
-    have_cython = True
-
-extra_compile_args = [
-    "-Wall",
-    "-O3",
-    "-Wextra",
-    "-m64",
-    "-ffast-math",
-    "-fno-finite-math-only",
-    "-march=native",
-    "-funroll-loops",
-]
-
-ext = "pyx" if have_cython else "c"
 ext_modules = [
     Extension(
         "cwinpy.heterodyne.fastheterodyne",
         sources=[
-            "cwinpy/heterodyne/fastheterodyne.{}".format(ext),
+            "cwinpy/heterodyne/fastheterodyne.pyx",
         ],
         include_dirs=[
-            numpy.get_include(),
+            np.get_include(),
             "cwinpy/heterodyne",
         ],
         libraries=["m"],
-        extra_compile_args=extra_compile_args,
+        extra_compile_args=[
+            "-Wall",
+            "-O3",
+            "-Wextra",
+            "-m64",
+            "-ffast-math",
+            "-fno-finite-math-only",
+            "-march=native",
+            "-funroll-loops",
+        ],
     )
 ]
-
-if have_cython:
-    from Cython.Build import cythonize
-
-    ext_modules = cythonize(ext_modules)
-
 
 setup(
     use_scm_version=True,
